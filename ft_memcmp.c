2016e9c25a34aab29e1e7a5498e43bb8ff5fcdda
Nick Fuhrmann <n.fuhrmann@protonmail.com>
2024-11-22 07:17:14 +0100

    fix: check for null1

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,22 +6,28 @@
 /*   By: nfuhrman <nfuhrman@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 01:56:04 by nfuhrman          #+#    #+#             */
-/*   Updated: 2024/11/22 06:37:47 by nfuhrman         ###   ########.fr       */
+/*   Updated: 2024/11/22 07:16:58 by nfuhrman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-int	ft_memcmp(const void *s1, const void *s2, size_t size)
+int ft_memcmp(const void *s1, const void *s2, size_t size)
 {
-	const unsigned char	*p1 = (const unsigned char *)s1;
-	const unsigned char	*p2 = (const unsigned char *)s2;
+    const unsigned char *p1 = (const unsigned char *)s1;
+    const unsigned char *p2 = (const unsigned char *)s2;
+    
+    if ((s1 == NULL || s2 == NULL) && size > 0)
+    {
+        return -1;
+    }
+    
+    while (size--)
+    {
+        if (*p1 != *p2)
+            return (*p1 - *p2);
+        p1++;
+        p2++;
+    }
 
-	while (size--)
-	{
-		if (*p1 != *p2)
-			return (*p1 - *p2);
-		p1++;
-		p2++;
-	}
-	return (0);
+    return 0;
 }
